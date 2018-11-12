91e7347db8e77b8796f92ebcae6fb2194346ccba
Johanna P <zon.pauna@pp.inet.fi>
2018-11-12 12:44:58 +0200

    Added function to get current weather to database

--- application/models/Saasivu_model.php
+++ application/models/Saasivu_model.php
@@ -2,21 +2,29 @@
 class Saasivu_model extends CI_model
 {
   public function get_weather(){
-       $this->db->select('*');
-       $this->db->from('saatila');
-       return $this->db->get()->result_array();
-     }
+    $this->db->select('*');
+    $this->db->from('saatila');
+    return $this->db->get()->result_array();
+  }
 
-     public function get_vkpaiva(){
-       $this->db->select('vkpaiva');
-       $this->db->from('saatila');
-       return $this->db->get()->result_array();
-     }
+  public function get_vkpaiva(){
+    $this->db->select('vkpaiva');
+    $this->db->from('saatila');
+    return $this->db->get()->result_array();
+  }
 
-     public function get_anturidata(){
-       $this->db->select('saa, lampo, sade, kosteus, valo');
-       $this->db->from('saatila');
-       return $this->db->get()->result_array();
-     }
+  public function get_anturidata(){
+    $this->db->select('saa, lampo, sade, kosteus, valo');
+    $this->db->from('saatila');
+    return $this->db->get()->result_array();
+  }
+
+  public function get_nykytila(){
+    $this->db->select('saa, lampo, sade, kosteus, valo');
+    $this->db->from('saatila');
+    $this->db->order_by('idsaatila', 'desc');
+    $this->db->limit(1);
+    return $this->db->get()->result_array();
+  }
 
 }
