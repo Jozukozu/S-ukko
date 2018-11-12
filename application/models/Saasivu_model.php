5b355024a74c61d4790713dd038f9a9ab73bd170
Johanna P <zon.pauna@pp.inet.fi>
2018-11-12 12:31:31 +0200

    Added sensordata function to database

--- application/models/Saasivu_model.php
+++ application/models/Saasivu_model.php
@@ -7,11 +7,16 @@ class Saasivu_model extends CI_model
        return $this->db->get()->result_array();
      }
 
-  public function get_vkpaiva(){
-          $this->db->select('vkpaiva');
-          $this->db->from('saatila');
-          return $this->db->get()->result_array();
-        }
+     public function get_vkpaiva(){
+       $this->db->select('vkpaiva');
+       $this->db->from('saatila');
+       return $this->db->get()->result_array();
+     }
 
+     public function get_anturidata(){
+       $this->db->select('saa, lampo, sade, kosteus, valo');
+       $this->db->from('saatila');
+       return $this->db->get()->result_array();
+     }
 
 }
